2432404180ad210898b71bd453cda06b8bfbb521
Yakirbe <yaqir1@gmail.com>
2024-12-08 13:42:41 +0200

    init commit

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Add your repository name here
+  base: '/tierpoint5demo/',  // If your repo name is "tierpoint5demo"
+  // ... rest of your config
+}) 
